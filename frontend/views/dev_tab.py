d26f3855b544adeeaa48361a94f6bc7ec5c43afa
rickegss <ricksilvagomes1@gmail.com>
2025-11-25 12:14:59 -0300

    feat: nome dos colaboradores

--- frontend/views/dev_tab.py
+++ frontend/views/dev_tab.py
@@ -4,12 +4,13 @@ def criar_aba_dev(page: ft.Page):
 
     backend = ft.Container(
         content=ft.Column([
-            ft.Text("Back-End (Lógica de Cálculos Estatísticos e Banco de Dados)", size=20, weight=ft.FontWeight.BOLD, color=ft.Colors.BLUE),
-            ft.Text("", size=18),
-            ft.Text("", size=18),
-            ft.Text("", size=18),
-            ft.Text("", size=18),
-            ft.Text("", size=18)
+            ft.Text("Back-End", size=22, weight=ft.FontWeight.BOLD, color=ft.Colors.BLUE),
+            ft.Text("(Lógica de Cálculos Estatísticos e Banco de Dados)", size=20, weight=ft.FontWeight.BOLD, color=ft.Colors.BLUE),
+            ft.Text("Pedro Henrique Silva do Nascimento", size=18),
+            ft.Text("Yuri Nascimento Pinheiro", size=18),
+            ft.Text("Kauã de Sousa Campos", size=18),
+            ft.Text("Wanderson Carvalho da Silva Júnior", size=18),
+            ft.Text("Paulo Henrique Andrade Chaveiro", size=18)
         ], alignment=ft.MainAxisAlignment.CENTER, horizontal_alignment=ft.CrossAxisAlignment.CENTER),
         padding=20,
         border=ft.border.all(2, ft.Colors.BLUE_200),
@@ -20,12 +21,13 @@ def criar_aba_dev(page: ft.Page):
 
     frontend = ft.Container(
         content=ft.Column([
-            ft.Text("Front-End (Visualização do Gráfico e Interface do Programa)", size=20, weight=ft.FontWeight.BOLD, color=ft.Colors.BLUE),
+            ft.Text("Front-End", size=22, weight=ft.FontWeight.BOLD, color=ft.Colors.BLUE),
+            ft.Text("(Visualização do Gráfico e Interface do Programa)", size=20, weight=ft.FontWeight.BOLD, color=ft.Colors.BLUE),
             ft.Text("Ricardo Gomes da Silva", size=18),
-            ft.Text("", size=18),
-            ft.Text("", size=18),
-            ft.Text("", size=18),
-            ft.Text("", size=18)
+            ft.Text("Weberth Silva de Souza", size=18),
+            ft.Text("Eric Faria dos Santos", size=18),
+            ft.Text("Pedro Henrique da Silva Sousa", size=18),
+            ft.Text("Brian Gustavo Rodrigues Ribeiro", size=18)
         ], alignment=ft.MainAxisAlignment.CENTER, horizontal_alignment=ft.CrossAxisAlignment.CENTER),
         padding=20,
             border=ft.border.all(2, ft.Colors.BLUE_200),
